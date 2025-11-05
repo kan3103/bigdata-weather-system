6a9ffbb09cb67e9268c39a589cdbe3e33b2acdfb
kan3103 <khang310304@gmail.com>
2025-11-05 13:25:21 +0700

    Add model LSTM to predict weather

---
+++ prediction/models/LSRMWeather.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+
+
+class LSTMWeather(nn.Module):
+    def __init__(self, input_dim, hidden_dim=128, num_layers=2, dropout=0.2):
+        super().__init__()
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=num_layers, batch_first=True, dropout=dropout)
+        self.fc = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
+
